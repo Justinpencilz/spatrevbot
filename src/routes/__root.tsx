2a9bf166ed55c669176baace00ece8aeabdb1321
Justinpencilz <Justinewhite158@gmail.com>
2026-04-24 00:27:59 +0100

    Refactor root route to use RootLayout
    Removed unused imports and the RootDocument component. Updated the route to use RootLayout as the shell component.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,7 +1,5 @@
-import { HeadContent, Scripts, createRootRoute } from '@tanstack/react-router'
-
-
-import '../styles.css'
+import { createRootRoute } from '@tanstack/react-router'
+import RootLayout from '../components/RootLayout'
 
 export const Route = createRootRoute({
   head: () => ({
@@ -18,36 +16,5 @@ export const Route = createRootRoute({
       },
     ],
   }),
-  shellComponent: RootDocument,
+  component: RootLayout,
 })
-
-function RootDocument({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <head>
-        <HeadContent />
-        <!-- Meta Pixel Code -->
-<script>
-!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1482904169891589');
-fbq('track', 'Lead');
-</script>
-<noscript><img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=1482904169891589&ev=PageView&noscript=1"
-/></noscript>
-<!-- End Meta Pixel Code -->
-      </head>
-      <body>
-        {children}
-        <Scripts />
-      </body>
-    </html>
-  )
-}
