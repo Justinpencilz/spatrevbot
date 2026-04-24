73ada8660e3b863af0efed0c195de720fd80f039
Justinpencilz <Justinewhite158@gmail.com>
2026-04-24 19:08:33 +0100

    Add Facebook tracking script to RootLayout

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -112,7 +112,8 @@ export default function RootLayout() {
           t.src=v;s=b.getElementsByTagName(e)[0];
           s.parentNode.insertBefore(t,s)}(window, document,'script',
           'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '1482904169891589');`}
+          fbq('init', '1482904169891589');
+          fbq('track', 'PageView');`}
         </script>
         <noscript>
           <img height="1" width="1" style={{display: 'none'}}
